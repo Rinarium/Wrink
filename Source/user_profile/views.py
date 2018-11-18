860c140a937adf2ab0866c8c413d423882312e4b
Egor Ptashnik <gogavp@mail.ru>
2018-11-18 22:39:42 +0300

    fix: disable error message after profile configuration

--- Source/user_profile/views.py
+++ Source/user_profile/views.py
@@ -17,9 +17,9 @@ def count_rating(user):
 
 def profile_page(request, profile_id):
     context = authenticated(request)
+    user_profile = request.user
     if request.method == 'POST' and 'save' in request.POST:
-        user_profile = request.user
-        user_form = UserForm(request.POST, instance=request.user)
+        user_form = UserForm(request.POST, instance=user_profile)
         profile_form = ProfileForm(request.POST, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -27,28 +27,27 @@ def profile_page(request, profile_id):
             messages.success(request, 'Your profile was successfully updated!')
         else:
             messages.error(request, 'Please correct the error below.')
-    else:
-        try:
-            profile = Profile.objects.get(pk=profile_id)
-            user_profile = profile.user
-            context['posts'] = Post.objects.filter(author=user_profile)
-        except ObjectDoesNotExist:
-            return redirect('home_page')
+    try:
+        profile = Profile.objects.get(pk=profile_id)
+        user_profile = profile.user
+        context['posts'] = Post.objects.filter(author=user_profile)
+    except ObjectDoesNotExist:
+        return redirect('home_page')
 
-        if request.method == 'POST':
-            if not request.user.is_authenticated:
-                messages.error(request, 'You must be signed in to like!')
-            elif request.user == user_profile:
-                messages.error(request, 'You cannot like yourself!')
-            elif user_profile.profile.voters.filter(user=request.user).exists():
-                messages.error(request, "You've already voted!")
-            elif 'plus' in request.POST:                                        # like
-                user_profile.profile.likes += 1
-                user_profile.profile.voters.add(request.user.profile)
-                user_profile.save()
-            else:
-                user_profile.profile.voters.add(request.user.profile)           # dislike
-                user_profile.save()
+    if 'plus' in request.POST or 'minus' in request.POST:
+        if not request.user.is_authenticated:
+            messages.error(request, 'You must be signed in to like!')
+        elif request.user == user_profile:
+            messages.error(request, 'You cannot like yourself!')
+        elif user_profile.profile.voters.filter(user=request.user).exists():
+            messages.error(request, "You've already voted!")
+        elif 'plus' in request.POST:                                        # like
+            user_profile.profile.likes += 1
+            user_profile.profile.voters.add(request.user.profile)
+            user_profile.save()
+        else:
+            user_profile.profile.voters.add(request.user.profile)           # dislike
+            user_profile.save()
 
     context['user_profile'] = user_profile
     context['rating'] = count_rating(user_profile)
